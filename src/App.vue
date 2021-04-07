62b520bfaa5c746f49aaa982bec87fc7b77da65c
hypergud <eowk3130@naver.com>
2021-04-08 03:48:57 +0900

    evt01-try01

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld2.vue'
+import HelloWorld2 from './components/HelloWorld2.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld2
   }
 }
 </script>
